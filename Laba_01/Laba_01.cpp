a4b0332f5ab1820844cc7b4399d803e5cd68da75
Michael (LomakinMN) <this@mail.ru>
2022-04-10 10:46:34 +0300

    code: проведена чистка кода

--- Laba_01/Laba_01.cpp
+++ Laba_01/Laba_01.cpp
@@ -156,113 +156,12 @@ int main()
     }
     // расчет гистограммы
 
-    /*double min = numbers[0];
-    double max = numbers[0];*/
-    /*double min;
-    double max;
-    find_minmax(numbers, min, max);*/
-    //cout << min << "   " << max << endl;
-    /*for (double number : numbers) {
-        if (min > number) {
-            min = number;
-        }
-        if (max < number) {
-            max = number;
-        }
-    }*/
-
-    /*vector<size_t> bins(bin_count, 0);
-    double bin_size = (max - min) / bin_count;
-    for (size_t i = 0; i < number_count; i++) {
-        bool found = false;
-        for (size_t j = 0; j < (bin_count - 1.0) && (!found) ; j++) {
-            auto lo = min + j * bin_size;
-            auto hi = min + (j + 1.0) * bin_size;
-            if (lo <= numbers[i] && (numbers[i] < hi)) {
-                bins[j]++;
-                found = true;
-            }
-        }
-        if (!found) {
-            bins[bin_count - 1]++;
-        };
-    } */
-
     const auto bins = make_histogram(numbers, bin_count,number_count);
 
     // вывод гистограммы 
 
     show_histogram_text(bins, scale_count);
 
-
-    //const size_t SCREEN_WIDTH = 80;
-    //const size_t MAX_ASTRERISK = SCREEN_WIDTH - 3 - 1 ;
-    //size_t max_bin = bins[0];//максимальная высота столбца
-    //for (size_t bin : bins) {
-    //    if (max_bin < bin) {
-    //        max_bin = bin;
-    //    }
-    //}
-   
-    //size_t max_height = 0;
-    //bool flag = false;
-
-    //for (size_t bin : bins) {
-    //    size_t height = bin;
-
-    //    if (max_bin > MAX_ASTRERISK) {
-    //        height = MAX_ASTRERISK * (static_cast<double>(bin) / max_bin);
-    //        flag = true;
-    //    }
-    //    if (height > max_height) {
-    //        max_height = height;
-    //    }
-
-    //    if (height  < 100) {
-    //        cout << " ";
-    //    }
-    //    if (height < 10) {
-    //        cout << " ";    
-    //    }
-    //    cout << height << "|";
-    //    for (size_t i = 0; i < height; i++) {
-    //        cout << "*";
-    //    }
-    //    cout << endl;
-    //}
-    //// создание шкалы
-    ////cout << endl;
-    //cout << "   |" ;
-    //if (flag) {
-    //    max_bin = max_height;
-    //}
-
-    //for (size_t i = 0; i < max_bin; i += scale_count) {
-    //    for (int j = 0; j < (scale_count - 1.0); j++) {
-    //        cout << "-";
-    //    };
-    //    cout << "|";
-    //};
-    //cout << endl;
-    //cout << "   0";
-    //for (size_t i = 0; i < (scale_count - 1.0); i++) {
-    //    cout << " ";
-    //}
-
-    //cout  << scale_count;
-    //if (max_bin > scale_count) {
-    //    size_t counter;
-
-    //    for (counter = 2.0 * scale_count; counter < max_bin; counter += scale_count) {
-    //        for (size_t j = 0; j < scale_count; j++) {
-    //            cout << " ";
-    //        };
-    //    }
-    //    for (size_t i = 0; i < (scale_count - 1.0); i++) {
-    //        cout << " ";
-    //    }
-    //    cout << counter << endl;
-    //}
 }
 
 
